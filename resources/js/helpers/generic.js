85b52d052809cffb085bce3a849a7f18e7b13adb
Juan "dientuki" Farias <dientuki@gmail.com>
2020-05-05 18:40:18 -0300

    fix js

--- resources/js/helpers/generic.js
+++ resources/js/helpers/generic.js
@@ -1,12 +1,24 @@
-/* eslint-disable prefer-const */
-export function mergeObjects(defaults, custom) {
-  if (typeof defaults === 'undefined') {
-    throw new Error('"defaults" object must be given');
-  }
-  if (typeof defaults !== 'object' || (typeof custom !== 'undefined' && typeof custom !== 'object')) {
-    throw new Error('Args must be an object');
-  }
+/* eslint-disable no-mixed-operators */
+/**
+ * Check if a var is an object
+ * Taken from https://github.com/jashkenas/underscore/blob/master/underscore.js#L1354
+ * @param obj
+ * @returns {boolean}
+ */
+export function isObject(obj) {
+  const type = typeof obj;
 
+  return type === 'function' || type === 'object' && Boolean(obj);
+}
+
+/* eslint-disable prefer-const */
+/**
+ * Merge two objects
+ * @param defaults
+ * @param custom
+ * @returns {{}}
+ */
+export function mergeObjects(defaults, custom) {
   let final = {},
     propertyName;
 
@@ -15,7 +27,11 @@ export function mergeObjects(defaults, custom) {
   }
 
   for (propertyName in custom) {
-    final[propertyName] = custom[propertyName];
+    if (isObject(custom[propertyName]) && final[propertyName] !== undefined) {
+      final[propertyName] = mergeObjects(defaults[propertyName], custom[propertyName]);
+    } else {
+      final[propertyName] = custom[propertyName];
+    }
   }
 
   return final;
