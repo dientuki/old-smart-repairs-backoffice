ec8ca5aadfa023474d7aaec5181dd6c8641b6d97
Juan "dientuki" Farias <dientuki@gmail.com>
2020-05-05 18:40:25 -0300

    added test

--- resources/js/helpers/generic.test.js
+++ resources/js/helpers/generic.test.js
@@ -1,43 +1,56 @@
-import Generic  from './generic';  // our module
+import { isObject, mergeObjects } from './generic';
 
-describe('Generic helper', () => {
+describe('isObject', () => {
+  test('using a string', () => {
+    expect(isObject('hola')).toEqual(false);
+  });
 
-    let generic;
+  test('using a json', () => {
+    expect(isObject({ a: 1 })).toEqual(true);
+  });
+});
 
-    beforeEach(() => {
-        generic = new Generic();
-    });
+describe('mergeObjects', () => {
+  test('returns a simple merged object', () => {
+    const objectA = {
+        a: 1,
+        b: 1
+      },
+      objectB = {
+        b: 2,
+        c: 3
+      },
+      result = {
+        a: 1,
+        b: 2,
+        c: 3
+      };
 
-    test('should be and object', () => {
-        expect(typeof generic).toBe('object');
-    });
+    expect(mergeObjects(objectA, objectB)).toEqual(result);
+  });
 
-    describe('#merge_objets()', () => {
-        test('should have a merge_objects Method', () =>{
-            expect(typeof Generic.merge_objects).toBe('function');
-        });
+  test('returns a complex merged object', () => {
+    const objectA = {
+        a: 1,
+        b: {
+          a: 1,
+          b: 2
+        }
+      },
+      objectB = {
+        b: { j: 3 },
+        c: 3
+      },
+      result = {
+        a: 1,
+        b: {
+          a: 1,
+          b: 2,
+          j: 3
+        },
+        c: 3
+      };
 
-        test('mandatory default object', () => {
-            expect(() => {
-              Generic.merge_objects();
-            }).toThrowError(Error);
-        });
-
-        test('args must be an object', () => {
-            expect(() => {
-              Generic.merge_objects(5, 'hola');
-            }).toThrowError(Error);
-        });
-
-        test('args valid', () => {
-            expect(() => {
-              Generic.merge_objects({a:1,b:1}, {b:2,c:3});
-            }).not.toThrowError(Error);
-        });
-
-        test('returns a merged object', () => {
-            expect(Generic.merge_objects({a:1,b:1},{b:2,c:3})).toEqual({a:1,b:2,c:3});
-        });
-
-    });
+    expect(mergeObjects(objectA, objectB)).toEqual(result);
+  });
 });
