9daacf9c6a03f1c40cebd6d3089e2d297c8bcbba
Juan "dientuki" Farias <dientuki@gmail.com>
2020-05-06 16:51:38 -0300

    update routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/admin', function () {
-    return view('welcome');
+Route::group(['prefix' => 'admin'], function() {
+    Auth::routes(['register' => false]);
+
+    Route::group(['middleware' => 'auth'], function(){
+        //Route::get('dashboard.html', ['uses' => 'PackagesController@index', 'as' => 'dashboard']);
+        Route::get('dashboard.html', 'HomeController@index')->name('dashboard');
+        Route::resource('users', 'UsersController')->except(['show']);
+    });
 });
