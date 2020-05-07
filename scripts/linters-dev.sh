e77cb65b42dd16c24f04f0b86a3deb6f852cba85
Juan "dientuki" Farias <dientuki@gmail.com>
2020-05-07 01:06:34 -0300

    update linters

--- scripts/linters-dev.sh
+++ scripts/linters-dev.sh
@@ -3,3 +3,4 @@
 mkdir -p phpLinter
 
 docker-compose exec backend ./vendor/bin/phpmd app/,routes/,resources/views/,resources/lang/,tests/ html scripts/phpmd.xml > phpLinter/phpmd.html
+docker-compose exec backend ./vendor/bin/phpcs app/ config/ database/ routes/ resources/views/ resources/lang/ tests/ --standard=PSR2 > phpLinter/phpcs.txt
