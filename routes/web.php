d69727eed02754935f2ebd3564d74526cea68bbb
Juan "dientuki" Farias <dientuki@gmail.com>
2020-05-10 20:45:54 -0300

    update routes

--- routes/web.php
+++ routes/web.php
@@ -14,29 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group(['prefix' => 'admin'], function () {
-    //Auth::routes(['register' => false]);
-
-    // Authentication Routes
-    Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-    Route::post('login', 'Auth\LoginController@login');
-    Route::post('logout', 'Auth\LoginController@logout')->name('logout');
-    
-    // Password Reset Routes
-    Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-    Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-    Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
-
-    // Password Confirmation Routes
-    Route::get('password/confirm', 'Auth\ConfirmPasswordController@showConfirmForm')->name('password.confirm');
-    Route::post('password/confirm', 'Auth\ConfirmPasswordController@confirm');
-
-    // Email Verification Routes
-    /*
-    Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
-    Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
-    Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
-    */
+    Auth::routes(['register' => false]);
 
     Route::group(['middleware' => 'auth'], function () {
         //Route::get('dashboard.html', ['uses' => 'PackagesController@index', 'as' => 'dashboard']);
