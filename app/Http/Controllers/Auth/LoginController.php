e297060c6cb77eee668944cbece9e4fd99a70a1c
Juan "dientuki" Farias <dientuki@gmail.com>
2020-05-10 20:47:20 -0300

    fix phpcs

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -38,7 +38,8 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    public function username(){
+    public function username()
+    {
         return 'username'; // this string is column of accounts table which we are going use for login
-    }    
+    }
 }
