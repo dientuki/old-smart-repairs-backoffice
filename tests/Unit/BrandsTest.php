faf98922cce8f142f7f2a5eca631c63a7604038d
Juan "dientuki" Farias <dientuki@gmail.com>
2020-05-22 01:40:16 -0300

    simple view

--- tests/Unit/BrandsTest.php
+++ tests/Unit/BrandsTest.php
@@ -2,9 +2,35 @@
 
 namespace Tests\Unit;
 
+use App\Brands;
 use Tests\TestCase;
 
 class BrandsTest extends TestCase
 {
+    protected $brands;
 
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->brands = new Brands();
+    }
+
+    public function testGetAscOrder()
+    {
+        $array = ['C','B','A'];
+
+        foreach ($array as $letter) {
+            factory(Brands::class)->create([
+                'brand' => $letter
+            ]);
+        }
+
+        $array = array_reverse($array);
+
+        $brands = $this->brands->getAll();
+
+        foreach ($brands as $key => $brand) {
+            $this->assertEquals($brand->brand, $array[$key]);
+        }
+    }
 }
