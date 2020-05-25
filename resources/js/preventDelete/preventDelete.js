c90e82a5ddf31ecee026b05d5ea6578d994cb341
Juan "dientuki" Farias <dientuki@gmail.com>
2020-05-25 02:01:32 -0300

    eslint

--- resources/js/preventDelete/preventDelete.js
+++ resources/js/preventDelete/preventDelete.js
@@ -9,39 +9,46 @@ export function preventDelete() {
 
   Array.from(document.querySelectorAll('.modalOpener')).forEach((form) => {
     form.addEventListener('submit', (event) => {
-      
+
       const confirmModal = new Modal({
-          title:  modalInfo.dataset.title,
-          content: modalInfo.dataset.content,
-          construct: true,
-          headerClose: true,
-          buttons: [
-            {text: modalInfo.dataset.cancel,
-              value: false,
-              attr: {
-                'class': 'btn btn-info',
-                'data-dismiss': 'modal'
-              }
+        buttons: [
+          {
+            attr: {
+              class: 'btn btn-info',
+              'data-dismiss': 'modal'
             },
-            {text: modalInfo.dataset.confirm,
-              value: true,
-              attr: {
-                'class': 'btn btn-danger',
-                'data-dismiss': 'modal'
-              }
-            }
-          ]
-        }
+            text: modalInfo.dataset.cancel,
+            value: false
+          },
+          {
+            attr: {
+              class: 'btn btn-danger',
+              'data-dismiss': 'modal'
+            },
+            text: modalInfo.dataset.confirm,
+            value: true
+          }
+        ],
+        construct: true,
+        content: modalInfo.dataset.content,
+        headerClose: true,
+        title: modalInfo.dataset.title
+      }
       );
-      confirmModal.on('show', function(confirmModal, event) {
-        console.log(confirmModal._html)
-        confirmModal._html.dialog.classList.add('modal-dialog-centered');
-        confirmModal._html.header.querySelector('h4').outerHTML = confirmModal._html.header.querySelector('h4').outerHTML.replace(/h4/g,"h5");
-      }).show().once('dismiss', function(modal, ev, button) {
-        if (button && button.value) {
-          event.target.submit();
+
+      confirmModal.on('show', (modal) => {
+        /* eslint-disable no-underscore-dangle */
+        modal._html.dialog.classList.add('modal-dialog-centered');
+        modal._html.header.querySelector('h4').outerHTML =
+          modal._html.header.querySelector('h4').outerHTML.replace(/h4/g, 'h5');
+        /* eslint-enable no-underscore-dangle */
+      }).show()
+        .once('dismiss', (modal, ev, button) => {
+          if (button && button.value) {
+            event.target.submit();
+          }
         }
-      });
+        );
 
       event.preventDefault();
     });
