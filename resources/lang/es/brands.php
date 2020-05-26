0557861b33b07d096c992ac775dbea999a3e0e1b
Juan "dientuki" Farias <dientuki@gmail.com>
2020-05-26 11:43:20 -0300

    added success alerts

--- resources/lang/es/brands.php
+++ resources/lang/es/brands.php
@@ -13,4 +13,7 @@ return [
     'brand' => 'marca|marcas',
     //'region_select_placeholder' => 'Seleccione una región...',
 
+    'store' => 'La marca ha sido creada con exito!.',
+    'update' => 'La marca ha sido actualizada con exito!.',
+    'destroy' => 'La marca ha sido borrada con exito!.'
 ];
