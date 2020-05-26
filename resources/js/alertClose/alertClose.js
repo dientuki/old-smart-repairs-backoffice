2a861410340ce54a23317b425a3aa9f433d85e23
Juan "dientuki" Farias <dientuki@gmail.com>
2020-05-26 02:49:42 -0300

    fix linter js

--- resources/js/alertClose/alertClose.js
+++ resources/js/alertClose/alertClose.js
@@ -2,8 +2,9 @@ export function alertClose(element) {
   if (element !== null) {
     element.addEventListener('click', (e) => {
       const animated = e.target.closest('.animated');
-      animated.classList.remove(animated.dataset['open'])
-      animated.classList.add(animated.dataset['close']);
+
+      animated.classList.remove(animated.dataset.open);
+      animated.classList.add(animated.dataset.close);
     });
   }
 }
