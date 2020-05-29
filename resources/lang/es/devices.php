82d4c77aa4b1dc6d34b22a6089e017631b091a84
Juan "dientuki" Farias <dientuki@gmail.com>
2020-05-29 02:56:25 -0300

    files

---
+++ resources/lang/es/devices.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Brands Language Lines
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'device' => 'equipo',
+    //'region_select_placeholder' => 'Seleccione una región...',
+
+    'store' => 'El tipo de equipo ha sido creada con exito!.',
+    'update' => 'El tipo de equipo ha sido actualizada con exito!.',
+    'destroy' => 'El tipo de equipo ha sido borrada con exito!.'
+];
