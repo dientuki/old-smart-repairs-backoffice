4a13c0b6f607db04b389dd784735b2e28a4079c3
Juan "dientuki" Farias <dientuki@gmail.com>
2020-05-30 23:18:55 -0300

    update seeder

--- database/seeds/DeviceTypeSeeder.php
+++ database/seeds/DeviceTypeSeeder.php
@@ -1,7 +1,6 @@
 <?php
 // phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
 
-use App\DeviceType;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -14,12 +13,8 @@ class DeviceTypeSeeder extends Seeder
      */
     public function run()
     {
-        factory(DeviceType::class, 50)->create()->each(function ($deviceType) {
-            $values = factory(DeviceType::class)->make();
-    
-            if (is_array($values)) {
-                $deviceType->save($values);
-            }
-        });
+        DB::table('device_types')->insert([
+            'device_type' => 'mobile'
+        ]);
     }
 }
--- database/seeds/DevicesSeeder.php
+++ database/seeds/DevicesSeeder.php
@@ -14,12 +14,28 @@ class DevicesSeeder extends Seeder
      */
     public function run()
     {
-        factory(Device::class, 50)->create()->each(function ($device) {
-            $values = factory(Device::class)->make();
-    
-            if (is_array($values)) {
-                $device->save($values);
-            }
-        });
+        DB::table('devices')->insert([
+            'tradename' => 'Blade A5',
+            'technical_name' => 'Blade A5',
+            'url' => 'https://www.gsmarena.com/zte_blade_a5_(2019)-9712.php',
+            'device_type_id' => 1,
+            'brand_id' => 1
+        ]);      
+
+        DB::table('devices')->insert([
+            'tradename' => 'Moto G7',
+            'technical_name' => 'XT1962',
+            'url' => 'https://www.gsmarena.com/motorola_moto_g7-9357.php',
+            'device_type_id' => 1,
+            'brand_id' => 2
+        ]);         
+
+        DB::table('devices')->insert([
+            'tradename' => 'Galaxy J2 Core',
+            'technical_name' => 'SM-J260M/DS',
+            'url' => 'https://www.gsmarena.com/samsung_galaxy_j2_core-9255.php',
+            'device_type_id' => 1,
+            'brand_id' => 3
+        ]);        
     }
 }
