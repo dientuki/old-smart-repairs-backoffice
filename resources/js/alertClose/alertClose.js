1ff73ebabc8cf56ade4bd8605d00b440de34a8d8
Juan "dientuki" Farias <dientuki@gmail.com>
2020-05-31 01:52:32 -0300

    alert close

--- resources/js/alertClose/alertClose.js
+++ resources/js/alertClose/alertClose.js
@@ -1,10 +1,12 @@
 export function alertClose(element) {
-  if (element !== null) {
-    element.addEventListener('click', (e) => {
-      const animated = e.target.closest('.animated');
-
-      animated.classList.remove(animated.dataset.open);
-      animated.classList.add(animated.dataset.close);
-    });
+  if (element === null) {
+    return false;
   }
+
+  element.addEventListener('click', (e) => {
+    const animated = e.target.closest('.animated');
+
+    animated.classList.remove(animated.dataset.open);
+    animated.classList.add(animated.dataset.close);
+  });
 }
---
+++ resources/js/alertClose/alertClose.test.js
@@ -0,0 +1,29 @@
+/**
+ * @jest-environment jsdom
+ */
+import { alertClose } from './alertClose';
+
+describe('alertClose', () => {
+  test('returns false if null is given', () => {
+
+    expect(alertClose(null)).toBe(false);
+  });
+
+  test('close alert', () => {
+    const wrapper = document.createElement('div'),
+      button = document.createElement('button');
+
+    wrapper.dataset.open = 'pulse';
+    wrapper.dataset.close = 'backOutUp';
+    wrapper.classList.add('animated', wrapper.dataset.open);
+    button.classList.add('alert-close');
+
+    wrapper.appendChild(button);
+    document.body.appendChild(wrapper);
+
+    alertClose(document.querySelector('.alert-close'));
+
+    button.click();
+    expect(wrapper.className).toBe('animated ' + wrapper.dataset.close);
+  });
+});
