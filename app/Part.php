846825c343e85ac6ac196b83402ee17b508f64cf
Juan "dientuki" Farias <dientuki@gmail.com>
2020-06-07 01:47:10 -0300

    media conversion

--- app/Part.php
+++ app/Part.php
@@ -6,6 +6,7 @@ use Spatie\Image\Manipulations;
 use Spatie\MediaLibrary\HasMedia;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class Part extends Model implements HasMedia
 {
@@ -73,4 +74,17 @@ class Part extends Model implements HasMedia
     {
         return $this->orderBy('name')->pluck('name', 'id');
     }
+
+    /**
+     * Media conversion
+     *
+     * @param  \Spatie\MediaLibrary\MediaCollections\Models\Media $media
+     * @return void
+     */    
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this->addMediaConversion('backoffice')
+          ->fit(Manipulations::FIT_CROP, 120, 120)
+          ->optimize();         
+    }    
 }
